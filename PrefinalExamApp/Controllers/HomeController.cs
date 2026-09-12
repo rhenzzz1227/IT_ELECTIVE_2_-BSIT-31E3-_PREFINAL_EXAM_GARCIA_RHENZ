a2e52d1f40df40abf2c22bec929ed7ce9cd47b37
RHENZ <rhenzjosephgarcia23@gmail.com>
2026-09-12 18:21:13 +0800

    Answered Question #1

--- PrefinalExamApp/Controllers/HomeController.cs
+++ PrefinalExamApp/Controllers/HomeController.cs
@@ -1,24 +1,27 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PrefinalExamApp.Models;
 
-namespace PrefinalExamApp.Controllers;
-
-public class HomeController : Controller
+namespace PrefinalExamApp.Controllers
 {
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            var questions = new List<ExamQuestion>();
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            questions.Add(new ExamQuestion {
+    Number = 1,
+    QuestionText = "What is the main problem solved by using a database instead of an in-memory collection?",
+    Options = new List<string> {
+        "A. It makes C# code shorter",
+        "B. It prevents the application from restarting",
+        "C. It allows data to persist after the application stops",
+        "D. It removes the need for MVC"
+    },
+    CorrectAnswer = "C. It allows data to persist after the application stops",
+    Explanation = "Databases provide non-volatile storage, preserving data across restarts."
+});
+            return View(questions);
+        }
     }
 }
